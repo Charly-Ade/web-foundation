aa26c470b6ba04dbf2893621a213139c9942f522
Charly-Ade <charlesnwodo77@gmail.com>
2026-05-05 20:13:16 +0100

    this javascript file is about variables in javascript

---
+++ 0x02/0-variables.js
@@ -0,0 +1,17 @@
+/**
+ * variables declaration with var, let and const
+ * - var: function-scoped, can be re-declared and updated
+ * - let: block-scoped, can be updated but not re-declared
+ * - const: block-scoped, cannot be updated or re-declared
+ */
+ //using var
+var name = "Alice"
+console.log(name)
+
+//using let
+let age = 30
+console.log(age)
+
+//using const
+const PI = 3.14
+console.log(PI)
