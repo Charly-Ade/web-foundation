051820cdfb4bda51e05702d3b8c0d0bfc0237148
Charly-Ade <charlesnwodo77@gmail.com>
2026-05-06 02:20:41 +0100

    A file about arithmetic in javascript

---
+++ 0x02/2-arithmetic.js
@@ -0,0 +1,19 @@
+/**
+ * arithmetic.js
+ * additon:+
+ * subtraction:-
+ * multiplication:*
+ * division:/
+ * modulus:%
+ * exponentiation:**
+ */
+
+let a = 10;
+let b = 20;
+
+console.log(a+b)
+console.log(a-b)
+console.log(b*a)
+console.log(b/a)
+console.log(a%b)
+console.log(a**b)
