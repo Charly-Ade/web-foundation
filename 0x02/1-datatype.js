31ec9204d411ca0514938c36b5c44bac98ce0cf6
Charly-Ade <charlesnwodo77@gmail.com>
2026-05-06 02:18:19 +0100

    A file that is about datatypes in javascript

---
+++ 0x02/1-datatype.js
@@ -0,0 +1,29 @@
+/**
+ * data types in JavaScript
+ * - string: a sequence of characters, enclosed in single or double quotes
+ * - number: a numeric value, can be an integer or a floating-point number
+ * - boolean: a logical value that can be either true or false 
+ * - null: a special value that represents the absence of any object value
+ * - undefined: a special value that represents the absence of a value or an uninitialized variable
+ * - symbol: a unique and immutable primitive value that can be used as a key for object properties
+ * - object: a collection of properties, where each property is a key-value pair   
+ */
+
+// string
+let firstname = "Charles"
+let lastname = "Nwodo"
+
+// number
+const age = 25
+
+// boolean
+const ismarried = true
+
+//children
+const no_child= null
+
+console.log(typeof firstname)
+console.log(typeof lastname)
+console.log(typeof age)
+console.log(typeof ismarried)
+console.log(typeof no_child)
