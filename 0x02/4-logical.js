aea28be2778a4407fce8af2f67ea534b71e194fb
Charly-Ade <charlesnwodo77@gmail.com>
2026-05-06 12:18:42 +0100

    a file in javascript about logical  operators

---
+++ 0x02/4-logical.js
@@ -0,0 +1,12 @@
+/**
+ * logical operators are used to combine two or more conditions in a single statement. They are often used in control flow statements such as if, while, and for loops to determine the flow of execution based on multiple conditions.
+ */
+
+console.log(false && "Hello");
+console.log(true || "world");
+console.log("" || "default value");
+
+/**
+ * what is the output of the above code?
+ *  explain the behavior of the logical operators in each case.
+ */
