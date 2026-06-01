130d3383d63e9361a6e77d57cfdf2de5eda05ef3
Charly-Ade <charlesnwodo77@gmail.com>
2026-06-01 20:50:30 +0100

    finding the index of an element in an array

---
+++ 0x05/2-index.js
@@ -0,0 +1,11 @@
+/**
+ * task: 2-index
+ * finding the index of an element in an array
+ * @author: Rashnotech solutions
+ */
+
+const fruits = ["Apple", "Banana", "Cherry", "Orange", "Grapes"]
+
+console.log(fruits.indexOf("Cherry"))
+//console.log(fruits.indexof("Mango"))
+console.log(fruits[0])
