908587b12afee2016780b90942e43a6c1c8f849b
Charly-Ade <charlesnwodo77@gmail.com>
2026-06-01 20:49:01 +0100

    adding values into an array

---
+++ 0x05/1-add.js
@@ -0,0 +1,12 @@
+/**
+ * task: 1-add
+ * adding values into an array
+ * author: Rashnotech solutions
+ */
+
+const fruits = ["Apple", "Banana", "Cherry"]
+
+fruits.push("Orange", "Grapes")
+
+console.log(fruits)
+console.log(fruits.length)
