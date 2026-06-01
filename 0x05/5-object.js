cedc7c209d0a95611d03cc0d8453cc2b6bb227d1
Charly-Ade <charlesnwodo77@gmail.com>
2026-06-01 21:06:03 +0100

    a javascript file about creating an Object

---
+++ 0x05/5-object.js
@@ -0,0 +1,17 @@
+/**
+ * javascript object
+ * task 5: create an empty object user
+ * assign the value 30 to the property age of the object user
+ * print the value  of age to the console 
+ */
+
+const person = {}
+
+person.age = 30
+person.name = "Charles Nwodo"
+person.location = "checkpoint Mararaba"
+person.gender = "male"
+
+console.log(person)
+console.log(typeof person)
+console.log(person instanceof Object)
