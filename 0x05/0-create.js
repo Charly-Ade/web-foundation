91b073811957607df8ed8903d2603acb2701a870
Charly-Ade <charlesnwodo77@gmail.com>
2026-06-01 20:47:32 +0100

    creating a set of array

---
+++ 0x05/0-create.js
@@ -0,0 +1,15 @@
+/**
+ * creating a set of array
+ * author Rashnotech solution
+ */
+
+const fruits = []
+const cars = Array()
+
+console.log(typeof fruits)
+console.log(fruits instanceof Array)
+console.log(fruits.length)
+
+console.log(typeof cars)
+console.log(cars instanceof Array)
+console.log(cars.length)
