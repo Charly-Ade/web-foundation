e5c42f027a25e23195d7a73e308f19cbb3c67eb0
Charly-Ade <charlesnwodo77@gmail.com>
2026-06-01 21:07:44 +0100

    an object keys

---
+++ 0x05/6-keys.js
@@ -0,0 +1,19 @@
+/**
+ * 6-keys
+ */
+
+const student = {
+    name: "Emmanuel David",
+    age: 16,
+    subjects: {
+        maths: 65,
+        eng: 70,
+        bio: 50,
+        chem: 80,
+        phy: 60
+    },
+    class: "SS3"
+}
+
+console.log(`Object keys: ${Object.keys(student)}`)
+console.log(`Object values: ${Object.values(student)}`)
